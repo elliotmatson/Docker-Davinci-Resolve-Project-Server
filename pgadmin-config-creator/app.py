c75fc233feb511f3ce1d421eb378009b440d1dee
Elliot Matson <ematson5897@gmail.com>
2022-07-29 23:56:28 -0500

    3rd time's the charm

--- pgadmin-config-creator/app.py
+++ pgadmin-config-creator/app.py
@@ -2,12 +2,13 @@ import json
 import os
 import time
 
-with open('/config/servers.json', 'w') as f:
+with open("/config/servers.json", "w") as f:
     print("created " + f.name)
-    template_string =   ('{"Servers": {"1": {"Name": "DavinciResolve", '
-                        '"Group": "Servers", "Port": 5432, "Username": '
-                        '"postgres", "Host": "postgres", "SSLMode": "prefer", "MaintenanceDB": "postgres"}}}'
-                        )
+    template_string = (
+        '{"Servers": {"1": {"Name": "DavinciResolve", '
+        '"Group": "Servers", "Port": 5432, "Username": '
+        '"postgres", "Host": "postgres", "SSLMode": "prefer", "MaintenanceDB": "postgres"}}}'
+    )
     data = json.loads(template_string)
     data["Servers"]["1"]["Username"] = os.getenv("POSTGRES_USER")
     print("printing json to file...")
