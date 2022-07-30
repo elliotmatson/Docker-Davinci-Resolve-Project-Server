a9bd2416631fee3c717eaca7d490692ba52d4db9
Elliot Matson <ematson5897@gmail.com>
2022-07-29 23:47:06 -0500

    Fix linting

--- pgadmin-config-creator/app.py
+++ pgadmin-config-creator/app.py
@@ -4,12 +4,14 @@ import time
 
 with open('/config/servers.json', 'w') as f:
     print("created " + f.name)
-    template_string = '{"Servers": {"1": {"Name": "DavinciResolve", "Group": "Servers", "Port": 5432, "Username": "postgres", "Host": "postgres", "SSLMode": "prefer", "MaintenanceDB": "postgres"}}}'
+    template_string =   ('{"Servers": {"1": {"Name": "DavinciResolve", '
+                        '"Group": "Servers", "Port": 5432, "Username": ''
+                        '"postgres", "Host": "postgres", "SSLMode": "prefer", "MaintenanceDB": "postgres"}}}'
+                        )
     data = json.loads(template_string)
-    data["Servers"]["1"]["Username"]=os.getenv("POSTGRES_USER")
+    data["Servers"]["1"]["Username"] = os.getenv("POSTGRES_USER")
     print("printing json to file...")
     print(data)
     json.dump(data, f, indent=2)
 # sleep until GHA Healthchecks are complete
 time.sleep(125)
-
