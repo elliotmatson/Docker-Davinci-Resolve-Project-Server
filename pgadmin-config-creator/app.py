960536ac53a93b5b51ee65231c7973bef2ade08b
BGoodes <moullecjulienpro@gmail.com>
2024-08-30 14:43:56 +0200

    Fix default credentials

--- pgadmin-config-creator/app.py
+++ pgadmin-config-creator/app.py
@@ -2,6 +2,12 @@ import json
 import os
 import time
 
+# get environment variables
+postgres_user = os.getenv("POSTGRES_USER")
+pgadmin_email = os.getenv("PGADMIN_DEFAULT_EMAIL")
+pgadmin_password = os.getenv("PGADMIN_DEFAULT_PASSWORD")
+
+
 # quit if config file already exists
 if os.path.exists("/config/servers.json"):
     print("/config/servers.json already exists.")
@@ -13,11 +19,12 @@ else:
         template_string = (
             '{"Servers": {"1": {"Name": "DavinciResolve", '
             '"Group": "Servers", "Port": 5432, "Username": '
-            '"postgres", "Host": "postgres", "SSLMode": "prefer", "MaintenanceDB": "postgres"}}}'
+            '"postgres", "Host": "postgres", "SSLMode": "prefer", "MaintenanceDB": "postgres"}},'
+            '"Credentials": {"email": "%s", "password": "%s"}}' % (pgadmin_email, pgadmin_password)
         )
         data = json.loads(template_string)
         # fix username
-        data["Servers"]["1"]["Username"] = os.getenv("POSTGRES_USER")
+        data["Servers"]["1"]["Username"] = postgres_user
         print("Printing json to file...")
         print(data)
         # update config file
